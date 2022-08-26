765ab6a70e2cd1f6611d50cdafd2e5a310e6ca6c
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-25 21:33:15 -0400

    crrated api post route

--- routes/routes.js
+++ routes/routes.js
@@ -7,8 +7,17 @@ module.exports = app => {
         if (err) throw err;
         var notyes = JSON.partse(data);
     });
-
+//get api route
     app.get("/api/notes", function(req, res) {
         res.json(notes);
     });
+//steup post route
+    app.post("/api/notes", function(req, res) {
+        let newNote = req.body;
+        notes.push(newNote);
+        updateDb();
+        return console.log("added new note: "+newNote.title);
+    });
+
+    
 }
