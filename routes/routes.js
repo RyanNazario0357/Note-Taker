64b971268fac3fd7271221f385e56e991f212397
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-25 21:29:55 -0400

    start API Routes

--- routes/routes.js
+++ routes/routes.js
@@ -6,5 +6,9 @@ module.exports = app => {
     fs.readFile("./db/db.json","utf8", (err, data) => {
         if (err) throw err;
         var notyes = JSON.partse(data);
-    })
+    });
+
+    app.get("/api/notes", function(req, res) {
+        res.json(notes);
+    });
 }
