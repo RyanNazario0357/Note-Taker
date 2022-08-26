a690399d2877760d86db24d6a9aafe002822e362
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-25 20:27:33 -0400

    added code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,3 +3,13 @@ var $noteText = $(".note-textarea");
 var $saveNoteBtn = $(".save-note");
 var $newNoteBtn = $(".new-note");
 var $noteList = $(".list-container .list-group");
+
+var activeNote = {};
+
+var getNotes = function() {
+    return $.ajax({
+        url: "/api/notes",
+        method: "GET"
+    });
+};
+
